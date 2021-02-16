4aabcb0de8acf37da5239464dd2c9798a6aaed59
jbuyu <jbuyu@ymail.com>
2021-02-16 10:38:25 +0300

    misc fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,6 @@
 import "./App.css";
-import Dashboard from "./components/Dashboard";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Navigation} />
-        <Route path="/dash">
-          <Dashboard />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <div>Howdy</div>;
 }
 
 export default App;
