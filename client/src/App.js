45a2ac1f85a73d17e298ac293a739c10f98cb4d3
jbuyu <jbuyu@ymail.com>
2021-02-17 11:10:37 +0300

    sets up tailwind css on header

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 function App() {
   return (
-    <div className="antialiased bg-gray-200">
+    <div className="antialiased bg-gray-200 ">
       <Header />
     </div>
   );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,65 @@
 import React from "react";
 import Logo from "../icons/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
 const Header = () => {
+  const btnClick = () => {
+    // console.log("howdy");
+  };
   return (
-    <header className="lg;px-16 px-6 bg-white flex flex-wrap items-centerlg:py-0 py-2">
+    <header className="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
       <div className="flex-1 flex justify-between items-center">
         <a href="#">
-          <img className="h-1/5 w-1/5" src={Logo} alt="logo" />
+          <img className="h-1/6 w-1/6" src={Logo} alt="logo" />
+        </a>
+      </div>
+      <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
+        <FaBars
+          className="text-xl fill-current text-gray-900"
+          onClick={btnClick}
+        />
+      </label>
+      <input type="checkbox" className="hidden" id="menu-toggle" />
+      <div
+        className="hidden lg:flex lg:items-center lg:width-auto w-full"
+        id="menu"
+      >
+        <nav>
+          <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+            <li>
+              <a
+                className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+                href="#"
+              >
+                Features
+              </a>
+            </li>
+            <li>
+              <a
+                className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+                href="#"
+              >
+                Docs
+              </a>
+            </li>
+            <li>
+              <a
+                className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400"
+                href="#"
+              >
+                Support
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <a
+          href="#"
+          className=" lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 cursor-pointer"
+        >
+          <CgProfile
+            className="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400"
+            alt="profile-imag"
+          />
         </a>
       </div>
     </header>
