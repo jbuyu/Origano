0b2d459ca15572f4358ca7b443aabe494941cdb5
jbuyu <jbuyu@ymail.com>
2021-02-17 11:39:43 +0300

    adds footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="antialiased bg-gray-200 ">
       <Header />
       <Body />
+      <Body />
+      <Body />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,102 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <footer className="footer bg-white relative pt-1 border-blue-700">
+      <div className="container mx-auto px-6">
+        <div className="sm:flex sm:mt-8">
+          <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700 uppercase mb-2">
+                Footer header 1
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">
+                Footer header 2
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400 text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400 text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">
+                Footer header 3
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+              <span className="my-2">
+                <a
+                  href="#"
+                  className="text-indigo-400  text-md hover:border-indigo-800"
+                >
+                  link 1
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
