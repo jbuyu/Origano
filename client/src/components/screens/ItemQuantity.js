552389ce586cfe9622570fbdaec3f6c349f5b2b1
jbuyu <jbuyu@ymail.com>
2021-02-21 11:26:47 +0300

    adds select button

---
+++ client/src/components/screens/ItemQuantity.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ItemQuantity = (count) => {
+  return <div>{count}</div>;
+};
--- client/src/components/screens/Product.js
+++ client/src/components/screens/Product.js
@@ -7,6 +7,8 @@ import { css } from "@emotion/react";
 import { useDispatch, useSelector } from "react-redux";
 import { listProductDetails } from "../../actions/productActions";
 import ErrorMessage from "../ErrorMessage";
+import "./Product.css";
+import { ItemQuantity } from "./ItemQuantity";
 export const Product = ({ match }) => {
   const [qty, useQty] = useState(0);
   const dispatch = useDispatch();
@@ -24,6 +26,14 @@ export const Product = ({ match }) => {
   }, [dispatch, match]);
 
   let { image, name, price, countInStock, rating, description, alt } = product;
+
+  const setCount = (count) => {
+    // useQty(count);
+    console.log(count);
+  };
+  const makeCount = () => {
+    console.log("hey");
+  };
   return (
     <div>
       {loading ? (
@@ -87,6 +97,34 @@ export const Product = ({ match }) => {
                       </span>
                     </div>
                   </div>
+
+                  <div className="relative inline-flex">
+                    <svg
+                      className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 412 232"
+                    >
+                      <path
+                        d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
+                        fill="#648299"
+                        fillRule="nonzero"
+                      />
+                    </svg>
+                    <select
+                      onChange={(e) => setCount(e.target.value)}
+                      className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none mt-3"
+                    >
+                      {[...Array(countInStock).keys()].map((count, index) => (
+                        <option
+                          className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap mx-2 cursor-pointer "
+                          key={index}
+                          value={count}
+                        >
+                          {count}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
                   {countInStock > 0 ? (
                     <div className="flex items-center mt-6">
                       <button className="px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
@@ -97,7 +135,10 @@ export const Product = ({ match }) => {
                       </button>
                     </div>
                   ) : (
-                    <button className="mt-6 cursor-not-allowed px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">
+                    <button
+                      onClick={makeCount}
+                      className="mt-6 cursor-not-allowed px-8 py-2 bg-indigo-600 text-white text-sm font-medium rounded hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500"
+                    >
                       Order
                     </button>
                   )}
