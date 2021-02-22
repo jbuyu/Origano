e02ae73dff12bb852d13fefc93c44eb6f5f995b6
jbuyu <jbuyu@ymail.com>
2021-02-22 21:59:59 +0300

    sets up cart screen

--- client/src/components/screens/CartScreen.js
+++ client/src/components/screens/CartScreen.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { addToCart } from "../../actions/cartActions";
+import ErrorMessage from "../ErrorMessage";
 
-export const CartScreen = () => {
+export const CartScreen = ({ match, history, locaton }) => {
+  const productID = match.params.id;
   return <div>Cart Screen</div>;
 };
