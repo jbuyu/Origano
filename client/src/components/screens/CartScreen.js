b89f8ab2d29aa72f4b17d803ceacddde4226369e
jbuyu <jbuyu@ymail.com>
2021-02-24 17:10:20 +0300

    adds intial table setup

--- client/src/components/screens/CartScreen.js
+++ client/src/components/screens/CartScreen.js
@@ -24,12 +24,60 @@ export const CartScreen = ({ match, history, location }) => {
   return (
     <div className="container mx-auto mt-10">
       <div className="flex shadow-md my-10">
-        <div className="w-3/4 bg-white px-10 py-10">
+        <div className="w-full bg-white px-10 py-10">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
             <h2 className="font-semibold text-2xl">3 Items</h2>
           </div>
-          <div className="flex mt-10 mb-5">
+          <table className="w-full">
+            <thead className="flex mt-10 mb-5  w-full">
+              <tr className="py-2">
+                <th className="font-semibold text-gray-600 text-xs uppercase w-2/5 px-20">
+                  Product Details
+                </th>
+                <th className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center px-20">
+                  Quantity
+                </th>
+                <th className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center px-20">
+                  Price
+                </th>
+                <th className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center px-20">
+                  Total
+                </th>
+              </tr>
+            </thead>
+            <tbody className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+              {cartItems ? (
+                cartItems.map(
+                  ({ countInStock, image, name, price, product, qty }) => (
+                    <tr key={product} className="flex w-full">
+                      <td className="flex w-2/5">
+                        <div className="w-20">
+                          <img
+                            className="h-16 w-16 rounded-2xl"
+                            src={image}
+                            // alt={alt}
+                          />
+                        </div>
+                        <div className="flex flex-col justify-between ml-4 flex-grow">
+                          {name}
+                        </div>
+                      </td>
+
+                      <td className="px-16 py-4">{qty}</td>
+                      <td className="px-16 py-4">{price}</td>
+                      <td className="px-16 py-4">{qty * price}</td>
+                    </tr>
+                  )
+                )
+              ) : (
+                <p>
+                  <ErrorMessage>Couldn't find product</ErrorMessage>
+                </p>
+              )}
+            </tbody>
+          </table>
+          {/* <div className="flex mt-10 mb-5">
             <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
               Product Details
             </h3>
@@ -42,7 +90,7 @@ export const CartScreen = ({ match, history, location }) => {
             <h3 className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center">
               Total
             </h3>
-          </div>
+          </div> */}
           <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
             <div className="flex w-2/5">
               <div className="w-20">
@@ -90,105 +138,6 @@ export const CartScreen = ({ match, history, location }) => {
             </span>
             <span className="text-center w-1/5 font-semibold text-sm">
               $400.00
-            </span>
-          </div>
-
-          <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-            <div className="flex w-2/5">
-              <div className="w-20">
-                <img
-                  className="h-24"
-                  src="https://drive.google.com/uc?id=10ht6a9IR3K2i1j0rHofp9-Oubl1Chraw"
-                  alt=""
-                />
-              </div>
-              <div className="flex flex-col justify-between ml-4 flex-grow">
-                <span className="font-bold text-sm">Xiaomi Mi 20000mAh</span>
-                <span className="text-red-500 text-xs">Xiaomi</span>
-                <a
-                  href="#"
-                  className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-                >
-                  Remove
-                </a>
-              </div>
-            </div>
-            <div className="flex justify-center w-1/5">
-              <svg
-                className="fill-current text-gray-600 w-3"
-                viewBox="0 0 448 512"
-              >
-                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-
-              <input
-                className="mx-2 border text-center w-8"
-                type="text"
-                value="1"
-                onChange={changeQuant}
-              />
-
-              <svg
-                className="fill-current text-gray-600 w-3"
-                viewBox="0 0 448 512"
-              >
-                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-            </div>
-            <span className="text-center w-1/5 font-semibold text-sm">
-              $40.00
-            </span>
-            <span className="text-center w-1/5 font-semibold text-sm">
-              $40.00
-            </span>
-          </div>
-
-          <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-            <div className="flex w-2/5">
-              <div className="w-20">
-                <img
-                  className="h-24"
-                  src="https://drive.google.com/uc?id=1vXhvO9HoljNolvAXLwtw_qX3WNZ0m75v"
-                  alt=""
-                />
-              </div>
-              <div className="flex flex-col justify-between ml-4 flex-grow">
-                <span className="font-bold text-sm">Airpods</span>
-                <span className="text-red-500 text-xs">Apple</span>
-                <a
-                  href="#"
-                  className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-                >
-                  Remove
-                </a>
-              </div>
-            </div>
-            <div className="flex justify-center w-1/5">
-              <svg
-                className="fill-current text-gray-600 w-3"
-                viewBox="0 0 448 512"
-              >
-                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-              <input
-                className="mx-2 border text-center w-8"
-                type="text"
-                value="1"
-                onChange={changeQuant}
-              />
-
-              <svg
-                className="fill-current text-gray-600 w-3"
-                viewBox="0 0 448 512"
-              >
-                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-            </div>
-            <span className="text-center w-1/5 font-semibold text-sm">
-              $150.00
-            </span>
-            <span className="text-center w-1/5 font-semibold text-sm">
-              $150.00
             </span>
           </div>
 
