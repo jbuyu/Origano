b977786e146171512d5e44c1ba1f9420ef973378
jbuyu <jbuyu@ymail.com>
2021-03-03 09:29:42 +0300

    adds table

--- client/src/components/screens/CartScreen.js
+++ client/src/components/screens/CartScreen.js
@@ -27,7 +27,9 @@ export const CartScreen = ({ match, history, location }) => {
         <div className="w-full bg-white px-10 py-10">
           <div className="flex justify-between border-b pb-8">
             <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-            <h2 className="font-semibold text-2xl">3 Items</h2>
+            <h2 className="font-semibold text-2xl">
+              {qty > 1 ? `${qty} Items` : `${qty} Item`}
+            </h2>
           </div>
           <table className="w-full">
             <thead className="flex mt-10 mb-5  w-full">
@@ -35,38 +37,71 @@ export const CartScreen = ({ match, history, location }) => {
                 <th className="font-semibold text-gray-600 text-xs uppercase w-2/5 px-20">
                   Product Details
                 </th>
-                <th className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center px-20">
+                <th className="font-semibold  text-gray-600 text-xs uppercase w-2/5 text-center px-20 py-4">
                   Quantity
                 </th>
-                <th className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center px-20">
+                <th className="font-semibold  text-gray-600 text-xs uppercase w-2/5 text-center px-20 py-4">
                   Price
                 </th>
-                <th className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center px-20">
+                <th className="font-semibold  text-gray-600 text-xs uppercase w-2/5 text-center px-20 py-4">
                   Total
                 </th>
               </tr>
             </thead>
-            <tbody className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            <tbody className="flex mt-10 mb-5  w-full flex-row">
               {cartItems ? (
                 cartItems.map(
-                  ({ countInStock, image, name, price, product, qty }) => (
-                    <tr key={product} className="flex w-full">
+                  ({
+                    countInStock,
+                    image,
+                    name,
+                    brand,
+                    price,
+                    product,
+                    qty,
+                  }) => (
+                    <tr
+                      key={product}
+                      className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5 w-3/5"
+                    >
                       <td className="flex w-2/5">
                         <div className="w-20">
                           <img
-                            className="h-16 w-16 rounded-2xl"
+                            className="h-16 w-16 rounded-xl"
                             src={image}
                             // alt={alt}
                           />
                         </div>
                         <div className="flex flex-col justify-between ml-4 flex-grow">
-                          {name}
+                          <span className="text-sm text-center w-1/5 font-semibold text-sm">
+                            {name}
+                          </span>
+                          <span className="text-red-500 text-xs text-center w-1/5 ">
+                            {brand}
+                          </span>
+                          <a
+                            href="#"
+                            className="font-semibold hover:text-red-500 text-gray-500 text-xs text-center w-1/5 font-semibold text-sm"
+                          >
+                            Remove
+                          </a>
                         </div>
                       </td>
 
-                      <td className="px-16 py-4">{qty}</td>
-                      <td className="px-16 py-4">{price}</td>
-                      <td className="px-16 py-4">{qty * price}</td>
+                      <td className=" px-20 py-4 text-center w-1/5 font-semibold text-sm">
+                        <input
+                          className="mx-2 border text-center w-8"
+                          type="text"
+                          value={qty}
+                          onChange={changeQuant}
+                        />
+                      </td>
+                      <td className="text-center w-1/5 font-semibold text-sm px-20 py-4">
+                        {price}
+                      </td>
+                      <td className="text-center w-1/5 font-semibold text-sm px-20 py-4">
+                        {qty * price}
+                      </td>
                     </tr>
                   )
                 )
@@ -77,72 +112,8 @@ export const CartScreen = ({ match, history, location }) => {
               )}
             </tbody>
           </table>
-          {/* <div className="flex mt-10 mb-5">
-            <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
-              Product Details
-            </h3>
-            <h3 className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center">
-              Quantity
-            </h3>
-            <h3 className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center">
-              Price
-            </h3>
-            <h3 className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center">
-              Total
-            </h3>
-          </div> */}
-          <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-            <div className="flex w-2/5">
-              <div className="w-20">
-                <img
-                  className="h-24"
-                  src="https://drive.google.com/uc?id=18KkAVkGFvaGNqPy2DIvTqmUH_nk39o3z"
-                  alt=""
-                />
-              </div>
-              <div className="flex flex-col justify-between ml-4 flex-grow">
-                <span className="font-bold text-sm">Iphone 6S</span>
-                <span className="text-red-500 text-xs">Apple</span>
-                <a
-                  href="#"
-                  className="font-semibold hover:text-red-500 text-gray-500 text-xs"
-                >
-                  Remove
-                </a>
-              </div>
-            </div>
-            <div className="flex justify-center w-1/5">
-              <svg
-                className="fill-current text-gray-600 w-3"
-                viewBox="0 0 448 512"
-              >
-                <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-
-              <input
-                className="mx-2 border text-center w-8"
-                type="text"
-                value="1"
-                onChange={changeQuant}
-              />
-
-              <svg
-                className="fill-current text-gray-600 w-3"
-                viewBox="0 0 448 512"
-              >
-                <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-              </svg>
-            </div>
-            <span className="text-center w-1/5 font-semibold text-sm">
-              $400.00
-            </span>
-            <span className="text-center w-1/5 font-semibold text-sm">
-              $400.00
-            </span>
-          </div>
-
-          <a
-            href="#"
+          <Link
+            to="/"
             className="flex font-semibold text-indigo-600 text-sm mt-10"
           >
             <svg
@@ -152,7 +123,7 @@ export const CartScreen = ({ match, history, location }) => {
               <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
             </svg>
             Continue Shopping
-          </a>
+          </Link>
         </div>
 
         {/* <div id="summary" className="w-1/4 px-8 py-10">
