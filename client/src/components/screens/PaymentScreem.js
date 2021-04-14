0863fbaa9d5df665e555ef5894411ebd16e64b23
jbuyu <jbuyu@ymail.com>
2021-04-14 10:57:51 +0300

    adds payment logo

--- client/src/components/screens/PaymentScreem.js
+++ client/src/components/screens/PaymentScreem.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { savePaymentMethod } from "../../actions/cartActions";
 import { CheckoutStages } from "../CheckoutStages";
+import PaypalSvg from "../../icons/paypal.svg";
+import MpesaSvg from "../../icons/mpesa.svg";
 
 export const PaymentScreen = ({ history }) => {
   //redux
@@ -11,7 +13,7 @@ export const PaymentScreen = ({ history }) => {
   const { shippingAddress } = cart;
 
   //state
-  const [paymentMethod, setPaymentMethod] = useState('paypal');
+  const [paymentMethod, setPaymentMethod] = useState("paypal");
   if (!shippingAddress) {
     history.push("/shipping");
   }
@@ -33,47 +35,54 @@ export const PaymentScreen = ({ history }) => {
           <CheckoutStages step1 step2 step3 />
         </div>
         <div className="flex flex-row justify-center align-middle">
-          <h1 className="mt-4 font-semibold text-2xl border-b pb-6">Payment</h1>
-        </div>
-        <form onSubmit={submitHandler}>
-          <div className="mt-4 flex flex-col">
-            <span className="text-gray-700">Account Type</span>
-            <div className="mt-2">
-              <label className="inline-flex items-center">
-                <input
-                  type="radio"
-                  className="form-radio"
-                  name="paymentmethod"
-                  id="paypal"
-                  value="paypal"
-                  onChange={(e)=>{
-                      setPaymentMethod(e.target.value)
-                  }}
-                />
-                <span className="ml-2">Paypal</span>
-              </label>
-              <label className="inline-flex items-center ml-6">
-                <input
-                  type="radio"
-                  className="form-radio"
-                  name="paymentmethod"
-                  id="mpesa"
-                  value="mpesa"
-                  onChange={(e)=>{
-                    setPaymentMethod(e.target.value)
-                }}
-                />
-                <span className="ml-2">Mpesa</span>
-              </label>
-            </div>
+          <div className="flex flex-col">
+            <h1 className="mt-4 font-semibold text-2xl border-b pb-6">
+              Payment
+            </h1>
+            <form onSubmit={submitHandler}>
+              <div className="mt-4">
+                <label className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    className="form-radio"
+                    name="paymentmethod"
+                    id="paypal"
+                    value="paypal"
+                    onChange={(e) => {
+                      setPaymentMethod(e.target.value);
+                    }}
+                  />
+                  <span className="ml-2">
+                    <img className="h-9 w-9" src={PaypalSvg} alt="logo" />
+                  </span>
+                </label>
+                <label className="inline-flex items-center ml-6">
+                  <input
+                    type="radio"
+                    className="form-radio"
+                    name="paymentmethod"
+                    id="mpesa"
+                    value="mpesa"
+                    onChange={(e) => {
+                      setPaymentMethod(e.target.value);
+                    }}
+                  />
+                  <span className="ml-2">
+                    <img className="h-16 w-16 p-1" src={MpesaSvg} alt="logo" />
+                  </span>
+                </label>
+              </div>
+              <div className="flex flex-row justify-center align-middle">
+                <button
+                  type="submit"
+                  className="bg-indigo-500 font-semibold hover:bg-indigo-600 p-2 text-sm text-white uppercase mt-4  rounded-lg"
+                >
+                  Continue
+                </button>
+              </div>
+            </form>
           </div>
-          <button
-            type="submit"
-            className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-2 text-sm text-white uppercase w-1/3 rounded-lg"
-          >
-            Continue
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
