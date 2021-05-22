03bccb43189d09e8cc7c8bbac6ef0759890f7da6
jbuyu <jbuyu@ymail.com>
2021-05-22 12:06:24 +0300

    adds deleting completion request

--- client/src/components/screens/ProductListScreen.js
+++ client/src/components/screens/ProductListScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { listProducts } from "../../actions/productActions";
+import { listProducts, deleteProduct } from "../../actions/productActions";
 
 import { HashLoader } from "react-spinners";
 
@@ -15,9 +15,19 @@ import { Link } from "react-router-dom";
 export const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch();
 
+  //list
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
+  // delete
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
+
+  //auth
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -27,21 +37,37 @@ export const ProductListScreen = ({ history, match }) => {
     } else {
       history.push("/login");
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const createProductHandler = (product) => {
-      //del
-  };
-  const deleteHandler = (product) => {
     //del
-};
+  };
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure")) {
+      dispatch(deleteProduct(id));
+    }
+  };
   return (
     <>
       <div className="flex justify-self-end max-w-7xl">
-        <button onClick={createProductHandler} className=" bg-green-500 active:bg-green-700 flex ml-auto p-2 text-white font-bold rounded-md mb-2">Product 
-        <AiOutlineAppstoreAdd/>
+        <button
+          onClick={createProductHandler}
+          className=" bg-green-500 active:bg-green-700 flex ml-auto p-2 text-white font-bold rounded-md mb-2"
+        >
+          Product
+          <AiOutlineAppstoreAdd />
         </button>
       </div>
+      {loadingDelete && (
+        <div className="flex justify-center items-center mt-6">
+          <HashLoader />
+        </div>
+      )}
+      {errorDelete && (
+        <span className="flex justify-center items-center text-sm bg-red-300 rounded-lg px-4 py-2 mb-4">
+        {errorDelete}
+      </span>
+      )}
       {loading ? (
         <div className="flex justify-center items-center mt-6">
           <HashLoader />
@@ -77,58 +103,70 @@ export const ProductListScreen = ({ history, match }) => {
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200">
-                {products.map(({ _id, name, price, category, brand, image, countInStock }) => (
-                  <tr key={_id}>
-                    <td class="px-6 py-4">
-                      <div class="flex items-center space-x-3">
-                        <div class="inline-flex w-10 h-10">
-                          <img
-                            class="w-10 h-10 object-cover rounded-full"
-                            alt="User avatar"
-                            src={image}
-                          />
+                {products.map(
+                  ({
+                    _id,
+                    name,
+                    price,
+                    category,
+                    brand,
+                    image,
+                    countInStock,
+                  }) => (
+                    <tr key={_id}>
+                      <td class="px-6 py-4">
+                        <div class="flex items-center space-x-3">
+                          <div class="inline-flex w-10 h-10">
+                            <img
+                              class="w-10 h-10 object-cover rounded-full"
+                              alt="User avatar"
+                              src={image}
+                            />
+                          </div>
+                          <div>
+                            <p class="">Jane Doe</p>
+                            <p class="text-gray-500 text-sm font-semibold tracking-wide">
+                              <span className="text-gray-500"> count:</span>{" "}
+                              {countInStock}
+                            </p>
+                          </div>
                         </div>
-                        <div>
-                          <p class="">Jane Doe</p>
-                          <p class="text-gray-500 text-sm font-semibold tracking-wide">
-                            <span className="text-gray-500"> count:</span> {countInStock}
-                          </p>
-                        </div>
-                      </div>
-                    </td>
-                    {/* <td className="px-6 py-4">
+                      </td>
+                      {/* <td className="px-6 py-4">
                       <p className="">{_id}</p>
                     </td> */}
-                    <td className="px-6 py-4 text-center">{name}</td>
-                    <td className="px-6 py-4 text-center">{price}</td>
-                    <td className="px-6 py-4 text-center">{category}</td>
-                    <td className="px-6 py-4 text-center">
-                      {brand}
-                    </td>
+                      <td className="px-6 py-4 text-center">{name}</td>
+                      <td className="px-6 py-4 text-center">{price}</td>
+                      <td className="px-6 py-4 text-center">{category}</td>
+                      <td className="px-6 py-4 text-center">{brand}</td>
 
-                    <td className="px-6 py-4 text-center">
-                      <Link className="px-1" to={`/admin/product/${_id}/edit`}>
-                        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-2 rounded">
-                          <span className="inline-block px-1">Edit </span>
+                      <td className="px-6 py-4 text-center">
+                        <Link
+                          className="px-1"
+                          to={`/admin/product/${_id}/edit`}
+                        >
+                          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-2 rounded">
+                            <span className="inline-block px-1">Edit </span>
+                            <span className="inline-block px-1">
+                              <FaUserEdit />
+                            </span>
+                          </button>
+                        </Link>
+                        <button
+                          onClick={() => {
+                            deleteHandler(_id);
+                          }}
+                          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-2 rounded"
+                        >
+                          <span className="inline-block px-1">Delete</span>
                           <span className="inline-block px-1">
-                            <FaUserEdit />
+                            <AiFillDelete />
                           </span>
                         </button>
-                      </Link>
-                      <button
-                        onClick={() => {
-                          deleteHandler(_id);
-                        }}
-                        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-2 rounded"
-                      >
-                        <span className="inline-block px-1">Delete</span>
-                        <span className="inline-block px-1">
-                          <AiFillDelete />
-                        </span>
-                      </button>
-                    </td>
-                  </tr>
-                ))}
+                      </td>
+                    </tr>
+                  )
+                )}
               </tbody>
             </table>
           </div>
--- client/src/store.js
+++ client/src/store.js
@@ -5,6 +5,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import {
   productListReducer,
   productDetailsReducer,
+  productDeleteReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
 import {
@@ -21,6 +22,7 @@ import {orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyRed
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
