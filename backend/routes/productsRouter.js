6b9deb886b1d001c196e906cdc9ca1550b1879cf
jbuyu <jbuyu@ymail.com>
2021-05-22 11:39:45 +0300

    adds delete products

--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 const Router = express.Router();
 import { getProducts, getProductById } from "../controllers/productController.js";
+import {protect, admin} from '../middleware/authMiddleware.js'
+import {deleteProduct} from '../controllers/productController.js'
 
 Router.route("/").get(getProducts);
 
-Router.route("/:id").get(getProductById)
+Router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct)
 
 export default Router;
