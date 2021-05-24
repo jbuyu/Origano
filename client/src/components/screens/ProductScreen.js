a770b85fa17c57ca6d8305d0d9c5ee5126d898a4
jbuyu <jbuyu@ymail.com>
2021-05-24 15:07:05 +0300

    fixes on comment body and form

--- client/src/components/screens/ProductScreen.js
+++ client/src/components/screens/ProductScreen.js
@@ -30,9 +30,8 @@ export const ProductScreen = ({ match, history }) => {
   let { success: successProductReview, error: errorProductRevview } =
     productReviewCreate;
 
-    const userLogin = useSelector((state) => state.userLogin);
-    let { userInfo } = userLogin;
-    
+  const userLogin = useSelector((state) => state.userLogin);
+  let { userInfo } = userLogin;
 
   const override = css`
     display: block;
@@ -59,32 +58,28 @@ export const ProductScreen = ({ match, history }) => {
       ) : error ? (
         <ErrorMessage>{error}</ErrorMessage>
       ) : (
-        <main className="my-8">
+        <main className="my-2">
           <div className="container mx-auto px-6">
             <div className="md:flex md:items-center">
-              <div className="flex flex-row">
-                <div>
-                  <div className="w-full h-64 md:w-1/2 lg:h-96">
-                    {image ? (
-                      <img
-                        className="h-4/5 w-full rounded-3xl object-cover max-w-lg mx-auto"
-                        src={image}
-                        alt={alt}
-                      />
-                    ) : (
-                      <SyncLoader size={15} />
-                    )}
-                  </div>
-                  <div className="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
-                    <h3 className="text-gray-700 uppercase text-lg mt-2">
-                      {name}
-                    </h3>
-                    <span className="text-gray-500 mt-3">{`Ksh. ${price}`}</span>
-                    <hr className="my-3" />
-                    <div>{description}</div>
-                  </div>
+              <div className="flex flex-col md:flex-row">
+                <div className="w-full h-64 md:w-1/2 lg:h-96">
+                  {image ? (
+                    <img
+                      className="h-4/5 w-full rounded-3xl object-cover max-w-sm mx-auto"
+                      src={image}
+                      alt={alt}
+                    />
+                  ) : (
+                    <SyncLoader size={15} />
+                  )}
                 </div>
-                <div className="px-2">
+                <div className="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
+                  <h3 className="text-gray-700 uppercase text-lg mt-2">
+                    {name}
+                  </h3>
+                  <span className="text-gray-500 mt-3">{`Ksh. ${price}`}</span>
+                  <hr className="py-3" />
+                  <div className="w-3/4">{description}</div>
                   <div className="mt-2">
                     <label className="text-gray-700 text-sm" htmlFor="count">
                       STOCK
@@ -95,6 +90,8 @@ export const ProductScreen = ({ match, history }) => {
                       </span>
                     </div>
                   </div>
+                </div>
+                <div className="px-4">
                   <div className="mt-2">
                     <label className="text-gray-700 text-sm" htmlFor="count">
                       Quantity
@@ -132,7 +129,6 @@ export const ProductScreen = ({ match, history }) => {
                       </span>
                     </div>
                   </div>
-
                   {countInStock > 0 ? (
                     <div className="flex items-center mt-6">
                       <button
@@ -150,10 +146,55 @@ export const ProductScreen = ({ match, history }) => {
                       Order
                     </button>
                   )}
+                  <div className="flex mx-auto items-center justify-center shadow-lg mt-4  mb-4 max-w-lg">
+                    <form className="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+                      <div className="flex flex-wrap -mx-3 mb-6">
+                        <h2 className="px-4 pt-3 pb-2 text-gray-800 text-lg">
+                          Add a new comment
+                        </h2>
+                        <div className="w-full md:w-full px-3 mb-2 mt-2">
+                          <textarea
+                            className="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
+                            name="body"
+                            placeholder="Type Your Comment"
+                            required
+                          ></textarea>
+                        </div>
+                        <div className="w-full md:w-full flex items-start  px-3">
+                          <div className="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                            <svg
+                              fill="none"
+                              className="w-5 h-5 text-gray-600 mr-1"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                              />
+                            </svg>
+                            <p className="text-xs md:text-sm pt-px">
+                              Some HTML is okay.
+                            </p>
+                          </div>
+                          <div className="-mr-1">
+                            <input
+                              type="submit"
+                              className="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100"
+                              value="Post Comment"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          {/* </div> */}
         </main>
       )}
     </div>
