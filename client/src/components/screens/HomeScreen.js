6c66e50c458aba29ebc9c2752854cf3784423672
jbuyu <jbuyu@ymail.com>
2021-05-25 19:55:12 +0300

    sets up height with rems

--- client/src/components/screens/HomeScreen.js
+++ client/src/components/screens/HomeScreen.js
@@ -26,43 +26,43 @@ export const HomeScreen = ({ match }) => {
       ) : error ? (
         <ErrorMessage>{error}</ErrorMessage>
       ) : (
-        <div className="bg-gray-100 flex flex-row flex-wrap justify-center items-center mt-4">
+        <div className="bg-gray-50 flex flex-row flex-wrap justify-center items-center mt-4">
           {products &&
             products.map(
               ({ _id, name, image, description, rating, category, alt }) => {
                 return (
-                  <div class="flex flex-col bg-white antialiased text-gray-900 p-4">
+                  <div className="flex flex-col bg-white antialiased text-gray-900 p-4">
                     <div>
                       <img
-                        src="https://source.unsplash.com/random/350x350"
+                        src={image}
                         alt=" random imgee"
-                        class="w-full object-cover object-center rounded-lg shadow-md "
+                        className="w-80 h-80 object-cover object-center rounded-lg shadow-md "
                       />
 
-                      <div class="relative px-4 -mt-16">
-                        <div class="bg-white p-6 rounded-lg shadow-lg">
-                          <div class="flex items-baseline">
-                            <span class="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
+                      <div className="relative px-4 -mt-16">
+                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                          <div className="flex items-baseline">
+                            <span className="bg-teal-200 text-teal-800 text-xs px-2 inline-block rounded-full  uppercase font-semibold tracking-wide">
                               New
                             </span>
-                            <div class="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
+                            <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
                               2 baths &bull; 3 rooms
                             </div>
                           </div>
 
-                          <h4 class="mt-1 text-xl font-semibold uppercase leading-tight truncate">
+                          <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">
                             A random Title
                           </h4>
 
-                          <div class="mt-1">
+                          <div className="mt-1">
                             $1800
-                            <span class="text-gray-600 text-sm"> /wk</span>
+                            <span className="text-gray-600 text-sm"> /wk</span>
                           </div>
-                          <div class="mt-4">
-                            <span class="text-teal-600 text-md font-semibold">
+                          <div className="mt-4">
+                            <span className="text-teal-600 text-md font-semibold">
                               4/5 ratings{" "}
                             </span>
-                            <span class="text-sm text-gray-600">
+                            <span className="text-sm text-gray-600">
                               (based on 234 ratings)
                             </span>
                           </div>
