45c694e49dbee0adbb423f42ee2cb61e20253aa1
jbuyu <jbuyu@ymail.com>
2021-05-26 16:08:31 +0300

    changes link to mainfile

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
