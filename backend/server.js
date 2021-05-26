ccbfa3d226784f097af1c04dbbcc13f0052a06c4
jbuyu <jbuyu@ymail.com>
2021-05-26 12:09:48 +0300

    sets up heroku static file serving, builds on front

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ import express from "express";
 import consola from "consola";
 const PORT = process.env.PORT || 4000;
 import cors from "cors";
-import morgan from 'morgan'
+import morgan from "morgan";
 // import bodyParser from "body-parser";
 const app = express();
 
 // if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+app.use(morgan("dev"));
 // }
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
@@ -39,9 +39,22 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
-//error middlewares
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
+//error middlewares
 app.use(notFound);
 app.use(errorHandler);
 
