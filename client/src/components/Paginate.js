1f687796758b471adce78e7c2837cd710693fdd6
jbuyu <jbuyu@ymail.com>
2021-05-26 07:52:49 +0300

    reqrites how links are written

--- client/src/components/Paginate.js
+++ client/src/components/Paginate.js
@@ -5,29 +5,32 @@ import { Link } from "react-router-dom";
 export const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
   return (
     pages > 1 && (
-      <ul className="flex">
+      <div className="flex">
         {[...Array(pages).keys()].map((x, index) => (
-          <li key={index} className="mx-1 px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg cursor-pointer">
-            <Link
-              className="font-bold"
-              key={x + 1}
-              to={
-                !isAdmin
-                  ? keyword
-                    ? `/search/${keyword}/page/${x + 1}`
-                    : `/page/${x + 1}`
-                  : `/admin/productList/${x + 1}`
-              }
+          <Link
+            className="font-bold"
+            key={x + 1}
+            to={
+              !isAdmin
+                ? keyword
+                  ? `/search/${keyword}/page/${x + 1}`
+                  : `/page/${x + 1}`
+                : `/admin/productList/${x + 1}`
+            }
+          >
+            <div
+              key={index}
+              className="mx-1 px-4 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg cursor-pointer"
             >
               {x + 1 === page ? (
                 <span className="text-red-400">{x + 1}</span>
               ) : (
                 <span>{x + 1}</span>
               )}
-            </Link>
-          </li>
+            </div>
+          </Link>
         ))}
-      </ul>
+      </div>
     )
   );
 };
